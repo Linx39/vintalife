aebd85d52c1903d18feaaa1106dbc4401f730b0e
Linx <olga-reshetova@mail.ru>
2024-05-11 15:07:14 +0700

    улучшена адаптивность слайдера

--- source/js/catalog-slider.js
+++ source/js/catalog-slider.js
@@ -1,45 +1,57 @@
 import { Width } from "./const.js";
 
+const CATALOG_CLASS = 'catalog';
 const CATALOG_SLIDER_CLASS = 'catalog__slider';
-// const CATALOG_SLIDER_NO_JS_CLASS = 'catalog__slider--no-js';
+const CATALOG_NO_JS_CLASS = 'catalog--no-js';
 
-const catalogSliderElement = document.querySelector(`.${CATALOG_SLIDER_CLASS}`);
+const catalogElement = document.querySelector(`.${CATALOG_CLASS}`);
 
-// catalogSliderElement.classList.remove(CATALOG_SLIDER_NO_JS_CLASS);
+if (catalogElement.classList.contains(CATALOG_NO_JS_CLASS)) {
+  catalogElement.classList.remove(CATALOG_NO_JS_CLASS);
+}
 
 const catalogSlider = new Swiper(`.${CATALOG_SLIDER_CLASS}`, {
   direction: 'horizontal',
   loop: true,
   slidesPerView: 'auto',
-  slidesPerGroup: 1,
-  spaceBetween: 10,
+  spaceBetween: 30,
+  slidesOffsetBefore: 30,
+  slidesOffsetAfter: 30,
+
+  // autoplay: {
+  //   delay: 3000,
+  //   pauseOnMouseEnter: true,
+  // },
 
   breakpoints: {
     [Width.MD]: {
       slidesPerView: 'auto',
-      slidesPerGroup: 2,
-      spaceBetween: 20,
+      spaceBetween: 30,
+      slidesOffsetBefore: 30,
+      slidesOffsetAfter: 30,
     },
     [Width.LG]: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 20,
-    },
-    [Width.XL]: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 30,
+      slidesPerView: 'auto',
+      spaceBetween: 40,
+      slidesOffsetBefore: 30,
+      slidesOffsetAfter: 30,
     },
     [Width.XXL]: {
       slidesPerView: 3,
-      slidesPerGroup: 3,
+      spaceBetween: 40,
+      slidesOffsetBefore: 0,
+      slidesOffsetAfter: 0,
+    },
+    [Width.XXXL]: {
+      slidesPerView: 3,
       spaceBetween: 60,
+      slidesOffsetBefore: 0,
+      slidesOffsetAfter: 0,
     },
   },
 
   navigation: {
     prevEl: '.catalog__slider-navigation-btn--prev',
     nextEl: '.catalog__slider-navigation-btn--next',
-    // disabledClass: 'needle-btn--disabled'
   },
 });
--- source/js/const.js
+++ source/js/const.js
@@ -2,9 +2,10 @@ export const Width = {
   MIN: 375,
   MAX: 1920,
 
-  SM: 515,
-  MD: 715,
+  SM: 520,
+  MD: 720,
   LG: 1010,
-  XL: 1190,
+  XL: 1260,
   XXL: 1560,
+  XXXL: 1720,
 }
