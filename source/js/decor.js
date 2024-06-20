3892bc5ad713a081898ceca39fedc14ce5aa5a2a
Linx <olga-reshetova@mail.ru>
2024-06-20 13:37:11 +0300

    сохранение

--- source/js/decor.js
+++ source/js/decor.js
@@ -1,32 +1,32 @@
-// const DECOR_CLASS = 'decor';
-// const DECOR_DARK_CLASS ='decor--dark'
+const DECOR_CLASS = 'decor';
+const DECOR_DARK_CLASS ='decor--dark'
 
-// const ELEMENTS_CLASS_ARRAY = ['main-nav', 'advantages', 'catalog', 'brands', 'credit', 'contacts'];
+const ELEMENTS_CLASS_ARRAY = ['main-nav', 'advantages', 'catalog', 'brands', 'credit', 'contacts'];
 
-// ELEMENTS_CLASS_ARRAY.forEach(className => {
-//   const element = document.querySelector(`.${className}`);
+ELEMENTS_CLASS_ARRAY.forEach(className => {
+  const element = document.querySelector(`.${className}`);
 
-//   const decor = document.createElement('div');
+  const decor = document.createElement('div');
 
-//   decor.classList.add(DECOR_CLASS);
-//   decor.classList.add(`${DECOR_CLASS}--${className}`);
+  decor.classList.add(DECOR_CLASS);
+  decor.classList.add(`${DECOR_CLASS}--${className}`);
 
-//   element.append(decor);
-// })
+  element.append(decor);
+})
 
-// export const addDecorDarkClass = () => {
-//   const decors = document.querySelectorAll(`.${DECOR_CLASS}`);
+export const addDecorDarkClass = () => {
+  const decors = document.querySelectorAll(`.${DECOR_CLASS}`);
 
-//   decors.forEach(decor => {
-//     decor.classList.add(DECOR_DARK_CLASS);
-//   })
-// };
+  decors.forEach(decor => {
+    decor.classList.add(DECOR_DARK_CLASS);
+  })
+};
 
 
-// export const removeDecorDarkClass = () => {
-//   const decors = document.querySelectorAll(`.${DECOR_CLASS}`);
+export const removeDecorDarkClass = () => {
+  const decors = document.querySelectorAll(`.${DECOR_CLASS}`);
 
-//   decors.forEach(decor => {
-//     decor.classList.remove(DECOR_DARK_CLASS);
-//   })
-// };
+  decors.forEach(decor => {
+    decor.classList.remove(DECOR_DARK_CLASS);
+  })
+};
