0c31306133af2a77b765a994e81284c1e1546c54
Linx <olga-reshetova@mail.ru>
2024-08-28 14:07:28 +0300

    change footer on new br p

--- source/js/site-menu.js
+++ source/js/site-menu.js
@@ -2,8 +2,8 @@ import { controlModal } from "./modal.js";
 
 const mainNav = document.querySelector('.main-nav');
 const siteMenu = mainNav.querySelector('.main-nav__site-menu');
-const openBtn = mainNav.querySelector('.main-nav__menu-open-btn');
-const closeBtns = mainNav.querySelectorAll('.main-nav__menu-close-btn');
+const openBtn = mainNav.querySelector('.main-nav__site-menu-open-btn');
+const closeBtns = mainNav.querySelectorAll('.main-nav__site-menu-close-btn');
 
 const {handleModal: handleSiteMenu} = controlModal(siteMenu, closeBtns);
 
