51a996f5c86ced59659ea02db5eea90a8c1d07a1
Linx <olga-reshetova@mail.ru>
2024-11-27 14:28:04 +0300

    fix form field validation

--- source/js/call-modal.js
+++ source/js/call-modal.js
@@ -1,5 +1,6 @@
 import { initModal } from "./modal.js";
+import { resetForms } from "./feedback.js";
 
 const call = document.querySelector('.call');
 
-const callModal = initModal(call);
+const callModal = initModal(call, null, resetForms);
--- source/js/feedback.js
+++ source/js/feedback.js
@@ -4,44 +4,46 @@ const MASK_OPTIONS = {
   mask: '+{7} (000) 000-00-00',
 };
 
-const NAME_PATTERN_MESAGE = 'Некорректное имя ;-)';
-const NAME_REQUIRED_MESAGE = 'Пожалуйста, заполните имя ;-)';
-const PHONE_PATTERN_MESAGE = 'Некорректный номер телефона ;)';
-const PHONE_REQUIRED_MESAGE = 'Пожалуйста, заполните номер телефона ;-)';
+const NAME_PATTERN_VALIDATE_MESAGE = 'Некорректное имя ;-)';
+const NAME_REQUIRED_VALIDATE_MESAGE = 'Пожалуйста, заполните имя ;-)';
+const PHONE_PATTERN_VALIDATE_MESAGE = 'Некорректный номер телефона ;)';
+const PHONE_REQUIRED_VALIDATE_MESAGE = 'Пожалуйста, заполните номер телефона ;-)';
 
 const feedbackForms = document.querySelectorAll('.feedback__form');
+let pristineArray = [];
 
-const resetAllForms = () => {
+const resetForms = () => {
   feedbackForms.forEach(form => form.reset());
+  pristineArray.forEach(pristine => pristine.reset());
 }
 
 feedbackForms.forEach(form => {
   const inputName = form.querySelector('.feedback__input--name');
   const inputPhone = form.querySelector('.feedback__input--phone');
 
-  inputName.setAttribute('data-pristine-pattern-message', NAME_PATTERN_MESAGE);
-  inputName.setAttribute('data-pristine-required-message', NAME_REQUIRED_MESAGE);
+  inputName.setAttribute('data-pristine-pattern-message', NAME_PATTERN_VALIDATE_MESAGE);
+  inputName.setAttribute('data-pristine-required-message', NAME_REQUIRED_VALIDATE_MESAGE);
 
-  inputPhone.setAttribute('data-pristine-pattern-message', PHONE_PATTERN_MESAGE);
-  inputPhone.setAttribute('data-pristine-required-message', PHONE_REQUIRED_MESAGE);
+  inputPhone.setAttribute('data-pristine-pattern-message', PHONE_PATTERN_VALIDATE_MESAGE);
+  inputPhone.setAttribute('data-pristine-required-message', PHONE_REQUIRED_VALIDATE_MESAGE);
 
-  let pristineConfig = {
+  const pristineConfig = {
     classTo: 'feedback__label',
-    errorClass: 'has-danger',
-    successClass: 'has-success',
     errorTextParent: 'feedback__label',
     errorTextTag: 'span',
-    errorTextClass: 'feedback__warning'
-};
+    errorTextClass: 'feedback__warning',
+  };
 
   const pristine = new Pristine(form, pristineConfig);
+
+  pristineArray.push(pristine);
 
   const mask = IMask(inputPhone, MASK_OPTIONS);
 
   form.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
-    let isFormValid = pristine.validate();
+    const isFormValid = pristine.validate();
 
     if(isFormValid) {
       setResponseSuccess();
@@ -51,7 +53,8 @@ feedbackForms.forEach(form => {
       setResponseError();
     }
 
-    resetAllForms();
-    pristine.reset();
+    resetForms();
   })
 })
+
+export {resetForms};
