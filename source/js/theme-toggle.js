10864d260609931fe14eb6f7a874a3bc247749f7
Linx <olga-reshetova@mail.ru>
2024-12-09 10:29:36 +0300

    fix logo

--- source/js/theme-toggle.js
+++ source/js/theme-toggle.js
@@ -4,17 +4,14 @@ const THEME_DARK_CLASS = 'dark-theme';
 
 const themeToggle = document.querySelector('.theme-toggle');
 const page = document.querySelector('.page');
-const logo = document.querySelector('.main-nav__logo img');
 
 const setLightTheme = () => {
   page.classList.remove(THEME_DARK_CLASS);
-  logo.src = './img/logo-dark.svg';
   removeDecorDarkClass();
 }
 
 const setDarkTheme = () => {
   page.classList.add(THEME_DARK_CLASS);
-  logo.src = './img/logo-light.svg';
   addDecorDarkClass();
 }
 
